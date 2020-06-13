be06752c40cdba84206a14648b28521bde09bb50
Lucas Furlan <luca.g.furlan@gmail.com>
2020-06-13 06:55:26 -0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Lucas Furlan on 13/06/2020.
+//  Copyright © 2020 Lucas Furlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
